64313a04133a44360f8440bb38f33465e2f2a9e7
YuraTheBest <78561809+YuraTheBest@users.noreply.github.com>
2023-11-18 16:46:42 +0300

    updated main.cpp

--- RoyalPenguin20-example/main.cpp
+++ RoyalPenguin20-example/main.cpp
@@ -1,23 +1,8 @@
-/*
-    ChibiOS - Copyright (C) 2006..2018 Giovanni Di Sirio
-
-    Licensed under the Apache License, Version 2.0 (the "License");
-    you may not use this file except in compliance with the License.
-    You may obtain a copy of the License at
-
-        http://www.apache.org/licenses/LICENSE-2.0
-
-    Unless required by applicable law or agreed to in writing, software
-    distributed under the License is distributed on an "AS IS" BASIS,
-    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-    See the License for the specific language governing permissions and
-    limitations under the License.
-*/
-
 #include "ch.hpp"
 #include "hal.h"
 #include "rt_test_root.h"
 #include "oslib_test_root.h"
+
 #include "modules/CppExampleLib/cpp_example.hpp"
 #include "modules/DebugLib/print_lib.hpp"
 
@@ -26,8 +11,6 @@
 #include "modules/MPU9250Lib/gyrodata.hpp"
 
 #include "stdio.h"
-
-using namespace chibios_rt; // FIXME DELETE IF NOT USED
 
 #define MPU9250_ADDRESS 0x68
 #define WHO_AM_I_RESP 0x73
@@ -44,20 +27,9 @@ using namespace chibios_rt; // FIXME DELETE IF NOT USED
 #define SPI_BaudRatePrescaler_128 ((uint16_t)0x0030) //  656.25 KHz  328.125 KHz
 #define SPI_BaudRatePrescaler_256 ((uint16_t)0x0038) //  328.125 KHz 164.06 KHz
 
-void spiCallback(void)
-{
-  palTogglePad(GPIOE, 15U);
-}
-
-/*
- * Green LED blinker thread, times are in milliseconds.
- */
 static THD_WORKING_AREA(waThread1, 128);
 static THD_FUNCTION(Thread1, arg)
 {
-
-  // (void)arg;
-  // chRegSetThreadName("blinker");
   MPU9250 *mpu = (MPU9250 *)arg;
   while (true)
   {
@@ -77,24 +49,6 @@ static THD_FUNCTION(Thread1, arg)
     float gy = gyroData.getAngularVelY();
     float gz = gyroData.getAngularVelZ();
 
-    int a = 1;
-
-    // stream << "Accel ";
-    // stream << "X : " << accelData.getAccelX();
-    // stream << "\n";
-    // printString(stream.str().c_str());
-
-    // BaseSequentialStream *chp = (BaseSequentialStream *)(&SDU1);
-    // char buff[128];
-    // sprintf(buff, "Accel [X : %.3f, Y : %.3f, Z : %.3f]\n", accelData.getAccelX(),
-    //         accelData.getAccelY(),
-    //         accelData.getAccelZ());
-    // sprintf(buff, "1234\n");
-    // printString(buff);
-    //  chprintf(chp, "Gyro [X : %.3f, Y : %.3f, Z : %.3f, timestamp: %d]\n", gyroData.getAngularVelX(),
-    //           gyroData.getAngularVelY(),
-    //           gyroData.getAngularVelZ(),
-    //           gyroData.getTimeStamp());
     chThdSleepMilliseconds(20);
   }
 }
@@ -113,8 +67,6 @@ int main(void)
    *   RTOS is active.
    */
   halInit();
-  System::init();             // FIXME DELETE IF NOT USED
-  chibios_rt::System::init(); // FIXME DELETE IF NOT USED
   chSysInit();
 
   /*
@@ -122,6 +74,9 @@ int main(void)
    */
   sdStart(&SD2, NULL);
 
+  /*
+   * SPI configuration
+   */
   SPIConfig spiConfig = {false, false, NULL, NULL, GPIOA, 4, SPI_BaudRatePrescaler_8, 0};
 
   /*
@@ -135,25 +90,19 @@ int main(void)
   palSetPad(GPIOA, 4);
   palSetPad(GPIOA, 2);
 
-  // Initialize the MPU9250 class
+  // MPU9250 class initialization
   MPU9250 mpu9250(&SPID1, &spiConfig);
 
   /*
-   * Creates the blinker thread.
+   * Creates new thread
    */
   chThdCreateStatic(waThread1, sizeof(waThread1), NORMALPRIO + 1, Thread1, &mpu9250);
 
   /*
-   * Normal main() thread activity, in this demo it does nothing except
-   * sleeping in a loop and check the button state.
+   * Normal main() thread activity, in this demo it does nothing
    */
   while (true)
   {
-    // if (!palReadPad(GPIOC, GPIOC_BUTTON))
-    // {
-    //   test_execute((BaseSequentialStream *)&SD2, &rt_test_suite);
-    //   test_execute((BaseSequentialStream *)&SD2, &oslib_test_suite);
-    // }
     chThdSleepMilliseconds(500);
   }
   return 0;
